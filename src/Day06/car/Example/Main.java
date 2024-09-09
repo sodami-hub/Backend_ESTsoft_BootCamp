23d5c284ca85962d91202933ef71ed2099faa8f6
good4ram@naver.com <good4ram@naver.com>
2024-09-09 11:40:53 +0900

    class day06.

---
+++ src/Day06/car/Example/Main.java
@@ -0,0 +1,9 @@
+package Day06.car.Example;
+
+import Day06.car.Car;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Day06/car/Example/SportsCar.java
@@ -0,0 +1,30 @@
+package Day06.car.Example;
+
+import Day06.car.Car;
+
+public class SportsCar extends Car {
+    SportsCar(String color) {
+        super(color);
+    }
+
+    public static void main(String[] args) {
+        Car car01 = new SportsCar("blue");
+        car01.model="kia";
+        /*
+        car01.color="ddddd";
+        System.out.println("car01.color = " + car01.color);
+        */
+
+        SportsCar car03 = (SportsCar)car01;
+
+        System.out.println(car03.color);
+        System.out.println();
+
+
+
+        SportsCar car02 = new SportsCar("aa");
+        car02.color ="white";
+        car02.model ="hyundai";
+        System.out.println(car02.color+" "+car02.model);
+    }
+}
