a6d2bb05a4c64f83c28d2f9abca2f6a760f99561
good4ram@naver.com <good4ram@naver.com>
2024-09-12 16:27:16 +0900

    class day10 add 9/11 DailyQuiz

---
+++ src/Day09/dailyquiz/NotExistIdException.java
@@ -0,0 +1,11 @@
+package Day09.dailyquiz;
+
+public class NotExistIdException extends Exception{
+    public NotExistIdException() {
+        super("존재하지 않는 id 입니다.");
+    }
+
+    public NotExistIdException(String message) {
+        super();
+    }
+}
