613dafd52144ee036e12b9fa20ed43877e55691a
good4ram@naver.com <good4ram@naver.com>
2024-09-20 16:53:46 +0900

    update weekly_quiz02

--- src/weekly_quiz_02/AddressBookExamMain.java
+++ src/weekly_quiz_02/AddressBookExamMain.java
@@ -1,12 +1,13 @@
 package weekly_quiz_02;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class AddressBookExamMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         AddressBook addr = new AddressBook();
-        int selecNum;
+        String selecNum;
         String name;
         String phoneNum;
         String company;
@@ -20,10 +21,10 @@ public class AddressBookExamMain {
             System.out.println("5. 종료");
             System.out.print("메뉴를 선택해주세요 > ");
 
-            selecNum = sc.nextInt();
+            selecNum = sc.next();
 
             switch(selecNum) {
-                case 1:
+                case "1":
                     System.out.print("이름을 입력하세요 :");
                     name = sc.next();
                     System.out.print("전화번호를 입력하세요 :");
@@ -32,7 +33,7 @@ public class AddressBookExamMain {
                     company = sc.next();
                     addr.contacts.add(new BusinessContact(name,phoneNum,company));
                     break;
-                case 2:
+                case "2":
                     System.out.print("이름을 입력하세요 : ");
                     name = sc.next();
                     System.out.print("전화번호를 입력하세요 :");
@@ -41,20 +42,22 @@ public class AddressBookExamMain {
                     relation = sc.next();
                     addr.contacts.add(new PersonalContact(name,phoneNum,relation));
                     break;
-                case 3:
+                case "3":
                     addr.displayContacts();
                     break;
-                case 4:
+                case "4":
                     System.out.print("검색할 이름을 입력하세요 : ");
                     name = sc.next();
                     addr.searchContact(name);
                     break;
-                case 5:
+                case "5":
                     break;
-
+                default:
+                    System.out.println("다시 입력하세요.");
+                    break;
             }
 
-            if(selecNum == 5){
+            if(selecNum.equals("5")){
                 System.out.println("종료합니다.");
                 break;
             }
