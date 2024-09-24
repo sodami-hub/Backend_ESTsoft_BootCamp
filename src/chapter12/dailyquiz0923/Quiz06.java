c3ab83df0ada5e40fa4a2ca075c4556d27a8b2d9
good4ram@naver.com <good4ram@naver.com>
2024-09-24 16:15:10 +0900

    dailyquiz0923 quiz07

--- src/chapter12/dailyquiz0923/Quiz06.java
+++ src/chapter12/dailyquiz0923/Quiz06.java
@@ -20,10 +20,12 @@ public class Quiz06 {
 
 
     public static void main(String[] args) {
-        double englishAvg = avg( s -> s.getEnglishScore());
+        //double englishAvg = avg( s -> s.getEnglishScore());
+        double englishAvg = avg(Student::getEnglishScore);
         System.out.println("영어 평균 점수: " + englishAvg);
 
-        double mathAvg = avg( s -> s.getMathScore() );
+        //double mathAvg = avg( s -> s.getMathScore() );
+        double mathAvg = avg(Student::getMathScore);
         System.out.println("수학 평균 점수: " + mathAvg);
     }
 
