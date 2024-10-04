9d16c21e042410a8d8785aa4946c6d48269b798b
good4ram@naver.com <good4ram@naver.com>
2024-10-04 10:34:39 +0900

    spring-demo-project RequestParam annotation

--- spring-demo-project/src/main/java/com/example/springdemoproject/controlller/HelloController.java
+++ spring-demo-project/src/main/java/com/example/springdemoproject/controlller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.springdemoproject.controlller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +11,9 @@ public class HelloController {
 //    }
 
     @GetMapping("/hello")
-    public String hello() {
-        return "hello, spring";
+    public String hello(@RequestParam(value="param", defaultValue = "Spring") String param) {
+        // RequestParam -> URI의 Query String을 가져와서 key(param)값을 찾음
+
+        return "hello," + param;
     }
 }
