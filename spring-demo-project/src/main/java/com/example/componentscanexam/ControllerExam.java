0aac7f16b58e8a1364d3540c1885f1cf72092b8f
good4ram@naver.com <good4ram@naver.com>
2024-10-04 16:18:43 +0900

    스프링 1일차 강의

---
+++ spring-demo-project/src/main/java/com/example/componentscanexam/ControllerExam.java
@@ -0,0 +1,9 @@
+package com.example.componentscanexam;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerExam {
+    // bese package가 아니기 때문에 컨테이너에 등록이 안됨.
+    // 컴포넌트 스캔을 위한 패키지 등록과 어노테이션등을 추가해야 됨.
+}
